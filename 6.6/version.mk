6c8ef80b5847106c20f601462736edd9e78ab4f6
Trevor Cooper <tcooper@sdsc.edu>
2015-02-16 09:37:15 -0800

    Changed default mirror location to a faster mirror

--- 6.6/version.mk
+++ 6.6/version.mk
@@ -3,7 +3,7 @@ ARCH          = x86_64
 VERSION       = 6.6
 DATE         :=$(shell date +%Y.%m.%d)
 ifndef MIRRORURL
-  MIRRORURL   =http://mirrors.kernel.org/centos
+  MIRRORURL   =http://mirror.hmc.edu/centos
 endif
 BASEPATH      = $(VERSION)/os/$(ARCH)/Packages
 UPDATESPATH   = $(VERSION)/updates/$(ARCH)/Packages
