48f832fb15e6588379eebb2924f72232e69b9ac5
Trevor Cooper <tcooper@sdsc.edu>
2015-06-17 10:46:28 -0700

    Removed 6.6 files from 6.5 branch

--- 6.6/version.mk
+++
@@ -1,9 +0,0 @@
-DISTRO        = CentOS
-ARCH          = x86_64
-VERSION       = 6.6
-DATE         :=$(shell date +%Y.%m.%d)
-ifndef MIRRORURL
-  MIRRORURL   =http://mirror.hmc.edu/centos
-endif
-BASEPATH      = $(VERSION)/os/$(ARCH)/Packages
-UPDATESPATH   = $(VERSION)/updates/$(ARCH)/Packages
