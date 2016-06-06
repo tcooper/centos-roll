f3690df79e63cbcf0253a3860d807233f0a39577
Trevor Cooper <tcooper@sdsc.edu>
2016-06-06 14:22:06 -0700

    Added support for CentOS 6.8

--- version.mk
+++ version.mk
@@ -1,9 +1,9 @@
 DISTRO        = CentOS
 ARCH          = x86_64
-VERSION       = 6.7
+VERSION       = 6.8
 DATE         :=$(shell date +%Y.%m.%d)
 ifndef MIRRORURL
-  MIRRORURL   =http://vault.centos.org
+  MIRRORURL   =http://mirror.hmc.edu/centos
 endif
 BASEPATH      = $(VERSION)/os/$(ARCH)/Packages
 UPDATESPATH   = $(VERSION)/updates/$(ARCH)/Packages
