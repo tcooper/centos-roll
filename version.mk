98dc1a017190a44fd8879d4b12fb8162bdef2152
Trevor Cooper <tcooper@sdsc.edu>
2017-10-02 19:18:20 -0700

    Switch mirror

--- version.mk
+++ version.mk
@@ -3,7 +3,7 @@ ARCH          = x86_64
 VERSION       = 6.9
 DATE         :=$(shell date +%Y.%m.%d)
 ifndef MIRRORURL
-  MIRRORURL   =http://mirror.hmc.edu/centos
+  MIRRORURL   =http://mirrors.ocf.berkeley.edu/centos
 endif
 BASEPATH      = $(VERSION)/os/$(ARCH)/Packages
 UPDATESPATH   = $(VERSION)/updates/$(ARCH)/Packages
