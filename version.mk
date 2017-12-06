4c65b852ef6c404c647b13d7f5790a658becffd3
Trevor Cooper <tcooper@sdsc.edu>
2017-12-06 14:17:31 -0800

    Implement more Rocks like version and release for roll

--- version.mk
+++ version.mk
@@ -3,7 +3,7 @@ ARCH          = x86_64
 VERSION       = 7.4.1708
 DATE         :=$(shell date +%Y.%m.%d)
 ifndef MIRRORURL
-  MIRRORURL   =http://mirror.hmc.edu/centos
+  MIRRORURL   =http://mirrors.ocf.berkeley.edu/centos
 endif
 BASEPATH      = $(VERSION)/os/$(ARCH)/Packages
 UPDATESPATH   = $(VERSION)/updates/$(ARCH)/Packages
